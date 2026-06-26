01e02290579ab07db0afcc5747572f2d59c5d7db
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-06-26 17:42:53 +0100

    modification au niveau du secret key

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -16,7 +16,7 @@ export const protectRoute = async (req, res, next) => {
 
     const token = authHeader.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "secret");
 
     if (!decoded) {
       return res.status(401).json({ message: "Unauthorized - Invalid token" });
